13b4ea2962f96df6e845f4eeaf7d0020a27d37d2
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-28 17:10:17 +0900

    contactsのダミーデータの日本語化

--- src/database/factories/ContactFactory.php
+++ src/database/factories/ContactFactory.php
@@ -14,18 +14,37 @@ class ContactFactory extends Factory
      *
      * @return array<string, mixed>
      */
+
     public function definition(): array
     {
+        // 性別を定義 (1:男性, 2:女性, 3:その他)
+        $gender = $this->faker->numberBetween(1, 3);
+
+        // 問い合わせサンプルを定義
+        $categories = [
+            1 => ['商品の発送状況について教えてください。', '商品のお届け日時の変更について相談したいです。'],
+            2 => ['商品の交換方法について教えてください。', '商品の交換条件について教えてください。'],
+            3 => ['注文内容とは別の商品が届きました。', '商品が故障していたので対応して頂きたいです。'],
+            4 => ['注文をキャンセルしたいのですが可能でしょうか。', 'サイトのメンテナンス時間を教えてください'],
+            5 => ['アカウント情報の変更について教えてください。', 'プロフィールの変更方法について教えてください'],
+        ];
+        $categoryId = $this->faker->numberBetween(1, 5);
+
         return [
-            'category_id' => $this->faker->numberBetween(1, 5),
-            'first_name' => $this->faker->firstName(),
+            'category_id' => $categoryId,
+            // 'first_name' => $this->faker->firstName(),
+            'first_name' => match ($gender) {
+                1 => $this->faker->firstNameMale(),
+                2 => $this->faker->firstNameFemale(),
+                3 => $this->faker->firstName(),
+            },
             'last_name' => $this->faker->lastName(),
-            'gender' => $this->faker->numberBetween(1, 3),
+            'gender' => $gender,
             'email' => $this->faker->safeEmail(),
             'tel' => $this->faker->numerify('0##########'),
-            'address' => $this->faker->address(),
+            'address' => $this->faker->prefecture() . ' ' . $this->faker->city() . ' ' . $this->faker->streetAddress(),
             'building' => $this->faker->secondaryAddress(),
-            'detail' => $this->faker->sentence(),
+            'detail' => $this->faker->randomElement($categories[$categoryId]),
         ];
     }
 }
