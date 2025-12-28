0fd62473e135d2845b0cc2cbea97bab567e38b02
yuta.yoshioka <yuta.yoshi.reps15@gmail.com>
2025-12-28 18:52:32 +0900

    UI用ファイルの準備

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Contact Form</title>
+    <link rel="stylesheet" href="{{asset('css/sanitize.css')}}">
+    <link rel="stylesheet" href="{{asset('css/common.css')}}">
+    @yield('css')
+</head>
+
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <div class="header__utilities">
+                <a href="/" class="header__log">FashionablyLate</a>
+            </div>
+            <nav class="header__nav">
+                @yield('header-nav')
+            </nav>
+        </div>
+    </header>
+
+    <main>
+        @yield('content')
+    </main>
+</body>
+
+</html>
